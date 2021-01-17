2c6b83ec7a6683b553493410f2d86c8e91b35002
Shalu Kumari <65071037+shalukumari123@users.noreply.github.com>
2021-01-17 23:33:17 +0530

    Add files via upload

---
+++ camerabootcamp.py
@@ -0,0 +1,36 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+while True:
+	ret,frame=cap.read()
+	gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+
+	if ret==False:
+		continue
+
+	faces = face_cascade.detectMultiScale(
+        gray_frame,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        
+    )
+
+
+	for(x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+	cv2.imshow("Video Frame",frame)
+
+
+
+	key_pressed=cv2.waitKey(1) & 0xFF
+
+	if key_pressed==ord('q'):
+		break
+
+cap.release()
+cv2.destroyALLWindows()
